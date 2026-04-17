80f14f5c337145a3e387c0102b61325afc799c15
aidenw-oss <aiden.ward@students.mq.edu.au>
2026-04-17 15:34:12 +1000

    Fix turn

--- Assets/Scripts/IconMove.cs
+++ Assets/Scripts/IconMove.cs
@@ -3,12 +3,11 @@ using UnityEngine;
 public class IconMove : MonoBehaviour
 {
     public float moveAmount = 0.5f;
-    public float turnAmount = 10f;
+    public float turnAmount = 90f;
 
     public void MoveForward()
     {
-        transform.position += new Vector3(moveAmount, 0f, 0f);
-        
+        transform.position += transform.right * moveAmount;
     }
 
     public void TurnLeft()
