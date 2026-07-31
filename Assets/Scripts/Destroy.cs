9c31a0927d6be4db695a0b368f7c2b0d6397b8bb
TosatXu <zihanhsu83@gmail.com>
2026-07-31 18:17:02 +1000

    encounter spawn fix

--- Assets/Scripts/Destroy.cs
+++ Assets/Scripts/Destroy.cs
@@ -6,6 +6,8 @@ public class Destroy : MonoBehaviour
     public Boolean overlap;
     public GameObject text;
     public GameObject monster;
+    public GameObject map;
+    public Vector3 spawnPoint;
 
     void OnTriggerStay2D(Collider2D collision)
     {
@@ -24,7 +26,7 @@ public class Destroy : MonoBehaviour
     {
         if (overlap)
         {
-            Instantiate(monster, this.transform);
+            Instantiate(monster, this.transform.position + spawnPoint, this.transform.rotation, map.transform);
             transform.DetachChildren();
             Instantiate(text);
             this.gameObject.SetActive(false);
